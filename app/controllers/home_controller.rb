143c01d37f3efef5c3c5da575014ef6a5d4ecb63
Serhii Hordiienko <phrippy2@gmail.com>
2023-03-06 01:02:50 +0200

    add post sorting on homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
     @tags = Tag.all.limit(5)
-    @posts = Post.all.limit(30)
+    @posts = Post.all.order(created_at: :desc).limit(30)
   end
 end
