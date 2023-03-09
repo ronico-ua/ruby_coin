c9daa9d4f498c3179bf33b9bfdfe88b38b8b155d
Serhii Hordiienko <phrippy2@gmail.com>
2023-03-09 03:46:28 +0200

    move Pagy::DEFAULT[:items] definition inside index method

--- app/controllers/admin/tags_controller.rb
+++ app/controllers/admin/tags_controller.rb
@@ -2,13 +2,13 @@
 
 module Admin
   class TagsController < ApplicationController
-    Pagy::DEFAULT[:items] = 6
 
     before_action :authorize_policy
     before_action :set_tag!, only: %i[destroy edit update]
     before_action { @pagy_locale = I18n.locale.to_s }
 
     def index
+      Pagy::DEFAULT[:items] = 6
       @tags = policy_scope(Tag).order(created_at: :desc)
       @pagy, @records = pagy(@tags)
     end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,9 @@
 class HomeController < ApplicationController
-  Pagy::DEFAULT[:items] = 9
 
   before_action { @pagy_locale = I18n.locale.to_s }
 
   def index
+    Pagy::DEFAULT[:items] = 9
     @tags = Tag.all.limit(5)
     @posts = Post.all.order(created_at: :desc)
     @pagy, @records = pagy(@posts)
