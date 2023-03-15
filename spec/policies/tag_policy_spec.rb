b73ae6662ad0df163f82459cbe4d278966ad25aa
Yaroslav Yenkala <yaroslavrick@gmail.com>
2023-03-15 12:08:44 +0200

    Commented empty specs

--- spec/policies/tag_policy_spec.rb
+++ spec/policies/tag_policy_spec.rb
@@ -9,19 +9,19 @@ RSpec.describe TagPolicy, type: :policy do
   #   pending "add some examples to (or delete) #{__FILE__}"
   # end
 
-  permissions :show? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
-
-  permissions :create? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
-
-  permissions :update? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
-
-  permissions :destroy? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
+  # permissions :show? do
+  #   pending "add some examples to (or delete) #{__FILE__}"
+  # end
+  #
+  # permissions :create? do
+  #   pending "add some examples to (or delete) #{__FILE__}"
+  # end
+  #
+  # permissions :update? do
+  #   pending "add some examples to (or delete) #{__FILE__}"
+  # end
+  #
+  # permissions :destroy? do
+  #   pending "add some examples to (or delete) #{__FILE__}"
+  # end
 end
