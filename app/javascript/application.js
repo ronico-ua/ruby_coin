0ea1435daac419d7438e60822c9df9d496dc983c
Danyil Shkoropad <31440971+anko20094@users.noreply.github.com>
2023-04-29 02:09:43 +0300

    Fix JS + update gems and JS plugins

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,10 +1,12 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
+import "./jquery"
+import "./toggle"
 import "./controllers"
-import * as ActiveStorage from "@rails/activestorage"
 
+import "@hotwired/turbo-rails"
 import 'bootstrap/js/dist/dropdown'
 import 'bootstrap/js/dist/collapse'
-import 'bootstrap5-toggle'
+import 'bootstrap5-toggle/js/bootstrap5-toggle.jquery'
+import * as ActiveStorage from "@rails/activestorage"
 
 ActiveStorage.start()
--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -2,49 +2,21 @@ import { Application } from "@hotwired/stimulus"
 
 const application = Application.start()
 
-import $ from 'jquery'
-
 // Configure Stimulus development experience
 application.debug = false
 window.Stimulus   = application
 
 export { application }
 
-document.addEventListener("DOMContentLoaded", function() {
-  var dropzone = document.getElementById("dropzone");
-
-  dropzone.addEventListener("dragover", function(e) {
-    e.preventDefault();
-    this.classList.add("dragover");
-  });
-
-  dropzone.addEventListener("dragleave", function(e) {
-    e.preventDefault();
-    this.classList.remove("dragover");
-  });
-
-  dropzone.addEventListener("drop", function(e) {
-    e.preventDefault();
-    this.classList.remove("dragover");
-
-    var files = e.dataTransfer.files;
-    var imageInput = document.getElementById("image-upload");
-    imageInput.files = files;
-  });
-});
-
-$(document).on('turbolinks:load', function() {
-  $('#toggle-one').bootstrapToggle();
-})
-
-$(document).on('turbolinks:load', function() {
-  function hideFlashMessage(setTime){
-    let time = 0
-    $($('.alert').get().reverse()).each(function() {
-      time = time + setTime
-      $(this).delay(time).fadeOut(300);
-    });
-  }
-
-  hideFlashMessage(3000)
-});
+// document.addEventListener("DOMContentLoaded", function() {
+//   function hideFlashMessage(setTime){
+//     let time = 0
+//     $($('.alert').get().reverse()).each(function() {
+//       time = time + setTime
+//       $(this).delay(time).fadeOut(300);
+//       // debugger;
+//     });
+//   }
+//
+//   hideFlashMessage(3000)
+// });
