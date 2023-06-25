7e1e39642bf482cbae32c3fba950bee434795ba7
Serhii Hordiienko <phrippy2@gmail.com>
2023-06-25 12:57:39 +0300

    Add tags filtration (#23)
    * Add tags filtration
    
    * Refactored
    
    ---------
    
    Co-authored-by: Danyil Shkoropad <anko20094@gmail.com>

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,14 @@
 
 class HomeController < ApplicationController
   def index
-    @tags = Tag.all.limit(5)
-    @posts = Post.all.order(created_at: :desc)
+    @tags = Tag.joins(:posts).distinct.limit(5)
+    @active_tags = params[:tags]
+    @all_posts = Post.active.order(created_at: :desc)
+    @posts = if @active_tags.present?
+               @all_posts.joins(:tags).where(tags: { title: @active_tags }).distinct
+             else
+               @all_posts
+             end
     @pagy, @posts = pagy(@posts, items: 6)
   end
 
