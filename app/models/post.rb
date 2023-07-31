5c667e41432fde494db1ed7f419a9fa947b02953
AndriiPuz <112422662+AndriiPuz@users.noreply.github.com>
2023-07-31 10:10:00 +0300

    implement button main post (#28)

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,10 +4,18 @@ class Post < ApplicationRecord
   has_and_belongs_to_many :tags
   belongs_to :user
   mount_uploader :photo, PhotoUploader
+  before_save :deactivate_previous_main_post, if: :main_post?
 
   enum status: { active: 0, inactive: 1 }
 
   def truncated_description
     description.truncate(100, separator: /\s/)
+  end
+
+  private
+
+  def deactivate_previous_main_post
+    previous_main_post = Post.find_by(main_post: true)
+    previous_main_post.update(main_post: false) if previous_main_post.present? && previous_main_post != self
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_01_194911) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_28_125849) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,6 +23,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_01_194911) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "main_post", default: false, null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
