f282d7ff690a9893ceac870a5d527c2b0aeb2838
Danyil Shkoropad <anko20094@gmail.com>
2023-09-19 23:06:54 +0300

    Small fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable, :confirmable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+  mount_uploader :avatar, PhotoUploader
 
   before_create :set_nickname
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,28 @@
 # frozen_string_literal: true
 
-User.create(email: 'test@test.com', password: 'test@test.com', nickname: 'test_user', role: 0) if User.all.blank?
+if User.first.blank?
+  user = User.create(
+    email: 'test@test.com',
+    password: 'test@test.com',
+    nickname: 'test_user',
+    role: 0,
+    remote_avatar_url: Faker::Avatar.image
+  )
 
-15.times do
-  Tag.create(title: Faker::Hipster.word)
-end
+  15.times do
+    Tag.create(title: Faker::Hipster.word)
+  end
 
-100.times do
-  Post.create(title: Faker::Hipster.sentence(word_count: 1),
-              subtitle: Faker::Lorem.paragraph(sentence_count: 3, supplemental: true,
-                                               random_sentences_to_add: 2),
-              description: Faker::Lorem.paragraph(sentence_count: 10, supplemental: true,
-                                                  random_sentences_to_add: 4),
-              status: :active,
-              user_id: 1,
-              tag_ids: Array.new(rand(5)) { rand(1...15) },
-              created_at: Faker::Time.backward(days: 1000))
+  50.times do
+    Post.create(title: Faker::Hipster.sentence(word_count: 1),
+                subtitle: Faker::Lorem.paragraph(sentence_count: 3, supplemental: true,
+                                                 random_sentences_to_add: 2),
+                description: Faker::Lorem.paragraph(sentence_count: 10, supplemental: true,
+                                                    random_sentences_to_add: 4),
+                status: :active,
+                user_id: user.id,
+                tag_ids: Array.new(rand(5)) { rand(1...15) },
+                created_at: Faker::Time.backward(days: 1000),
+                remote_photo_url: Faker::Avatar.image)
+  end
 end
