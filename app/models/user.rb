828848928555eeefffc7ef84b15b778aa5960331
AndriiAndriyovuch <andriy.mazurok.tl@gmail.com>
2023-09-26 21:44:51 +0300

    Made errors turbo frame and use it in registration path, added locales for errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,8 +12,9 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true, uniqueness: { case_sensitive: false }, on: :update
   validates :avatar, presence: true, on: :update
-  validates :email, presence: true, uniqueness: true, format: { with: Devise.email_regexp }
-  validates :encrypted_password, presence: true
+
+  validates :email, presence: true
+  validates :email, uniqueness: true, format: { with: Devise.email_regexp }, if: -> { email.present? }
 
   enum :role, { admin: 0, moderator: 1, user: 2 }
 
