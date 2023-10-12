f1553fe38f3e1bc4258c08fdc8ed1bb26777ccc6
Danyil Shkoropad <anko20094@gmail.com>
2023-10-13 00:11:35 +0300

    Try fix

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -11,30 +11,7 @@ require 'rspec/rails'
 require 'devise'
 require 'factory_bot_rails'
 require 'shoulda/matchers'
-Capybara.javascript_driver = :rack_test
-Shoulda::Matchers.configure do |config|
-  config.integrate do |with|
-    with.test_framework :rspec
-    with.library :rails
-  end
-end
 
-# Add additional requires below this line. Rails is not loaded until this point!
-
-# Requires supporting ruby files with custom matchers and macros, etc, in
-# spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
-# run as spec files by default. This means that files in spec/support that end
-# in _spec.rb will both be required and run as specs, causing the specs to be
-# run twice. It is recommended that you do not name files matching this glob to
-# end with _spec.rb. You can configure this pattern with the --pattern
-# option on the command line or in ~/.rspec, .rspec or `.rspec-local`.
-#
-# The following line is provided for convenience purposes. It has the downside
-# of increasing the boot-up time by auto-requiring all files in the support
-# directory. Alternatively, in the individual `*_spec.rb` files, manually
-# require only the support files necessary.
-#
-# Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
 Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
@@ -50,9 +27,6 @@ RSpec.configure do |config|
   end
   config.before(:each, type: :feature) do
     default_url_options[:locale] = I18n.default_locale
-  end
-  config.before(:each, type: :system) do
-    driven_by :selenium_headless
   end
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = Rails.root.join('spec', 'fixtures')
