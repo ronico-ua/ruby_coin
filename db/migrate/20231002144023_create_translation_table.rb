f8d6b2d96f20fb0f374807f8d35bddab5c98000c
AndriiAndriyovuch <andriy.mazurok.tl@gmail.com>
2023-10-15 12:09:55 +0300

    Removed down method cause if user make rollback he won't be able to migrate again

--- db/migrate/20231002144023_create_translation_table.rb
+++ db/migrate/20231002144023_create_translation_table.rb
@@ -4,19 +4,17 @@ class CreateTranslationTable < ActiveRecord::Migration[7.0]
   def up
     change_column_null :posts, :slug, true
 
-    Post.create_translation_table!({
-                                     title: :string,
-                                     subtitle: :string,
-                                     description: :string,
-                                     slug: :string
-                                   }, {
-                                     migrate_data: true
-                                   })
+    unless table_exists?(:post_translations)
+      Post.create_translation_table!({
+                                       title: :string,
+                                       subtitle: :string,
+                                       description: :string,
+                                       slug: :string
+                                     }, {
+                                       migrate_data: true
+                                     })
+    end
 
     Post.find_each(&:generate_slugs)
-  end
-
-  def down
-    Post.drop_translation_table!
   end
 end
