67d5b4af6a68cb239e08d6051224cb0e6e2cbbe0
Andrii <puzyrandrii@gmail.com>
2023-11-14 13:29:09 +0200

    fix tracker logic

--- app/validators/ahoy/visits_validator.rb
+++ app/validators/ahoy/visits_validator.rb
@@ -2,16 +2,16 @@
 
 module Ahoy
   class VisitsValidator < BaseValidator
-    attr_accessor :last_visit
+    attr_accessor :last_visit_for_post
 
     validate :first_visit_or_more_than_24_hours
 
     private
 
     def first_visit_or_more_than_24_hours
-      return if last_visit.nil?
+      return if last_visit_for_post.nil?
 
-      last_visit_time = Time.zone.at(last_visit)
+      last_visit_time = Time.zone.at(last_visit_for_post)
       time_ago = 24.hours.ago
 
       return if last_visit_time < time_ago
