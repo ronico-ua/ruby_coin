7d9791ccf22f220143dbc216f7f9cb01bc1d41f5
Andrii <puzyrandrii@gmail.com>
2023-11-14 14:39:00 +0200

    write test for statistic

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -35,7 +35,6 @@ describe HomeController, type: :request do
         expect(Ahoy::Event.count).to eq(0)
 
         get post_path(post)
-
         get post_path(post)
 
         expect(Ahoy::Visit.count).to eq(1)
@@ -55,6 +54,22 @@ describe HomeController, type: :request do
         get post_path(post)
 
         expect(Ahoy::Visit.count).to eq(2)
+        expect(Ahoy::Event.count).to eq(2)
+      end
+    end
+
+    context 'when look through different posts' do
+      let(:post_first) { create(:post, status: 'active') }
+      let(:post_second) { create(:post, status: 'active') }
+
+      it 'tracking two different posts within 24 hours' do
+        expect(Ahoy::Visit.count).to eq(0)
+        expect(Ahoy::Event.count).to eq(0)
+
+        get post_path(post_first)
+        get post_path(post_second)
+
+        expect(Ahoy::Visit.count).to eq(1)
         expect(Ahoy::Event.count).to eq(2)
       end
     end
