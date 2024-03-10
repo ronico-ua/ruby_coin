1d1761802450c6976bfdaa9eaa6c22f40212a5f9
Danyil Shkoropad <anko20094@gmail.com>
2024-03-10 11:45:42 +0200

    Updated the RSpec description in user_policy_spec.rb to accurately reference ActiveAdmin::UserPolicy

--- spec/policies/active_admin/user_policy_spec.rb
+++ spec/policies/active_admin/user_policy_spec.rb
@@ -2,7 +2,7 @@
 
 require 'rails_helper'
 require_relative '../../../app/policies/active_admin/user_policy'
-RSpec.describe UserPolicy do
+RSpec.describe ActiveAdmin::UserPolicy do
   subject(:policy) { described_class.new(user, record) }
 
   describe '#index?' do
